9f8c4b19dcd0f8c71f1f94f278261305378b04ce
IlyaKravchenk1 <90101572+IlyaKravchenk1@users.noreply.github.com>
2021-09-11 15:34:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+
+num = input('Введите трехзначное число:\n')
+
+if num.isdigit():
+    if len(num) != 3:
+        print('Нужно ввести 3х значное число.')
+    else:
+        mult = 1
+        summ = 0
+        for i in num:
+            summ += int(i)
+            mult *= int(i)
+        print(f'Сумма цифр цисла {num} = {summ}, произведение = {mult}')
+else:
+    print('Введите ЧИСЛО согласно условию')
+
+    if num create ground first
+
+a = input("сколько надо фунтов для покупки бокса")
+    if a > 5 ground groh near  create double gam    e(23-2)
+     ground as franklin
